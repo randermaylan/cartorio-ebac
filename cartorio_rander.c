577c43344d58cb28ff173c49788a4032edae9aa1
randermaylan <143401479+randermaylan@users.noreply.github.com>
2023-10-01 21:27:43 -0300

    Evolutivas em todas as opções
    Evolutivas: Inclusão de consulta ao usuário sobre a inserção, consulta e delete de outros usuários ou saída para o menu principal.

--- cartorio_rander.c
+++ cartorio_rander.c
@@ -5,11 +5,14 @@
 
 int registrar() //Função responsável pelo cadastro de usuários no sistema
 {
-	char arquivo[40]; //Início da criação de variáveis/string
+	//Início da criação de variáveis/string
+	char arquivo[40];
 	char cpf[40];
 	char nome[40];
 	char sobrenome[40];
-	char cargo[40]; //Final da criação de variáveis/string
+	char cargo[40]; 
+	char opcao; 
+	//Final da criação de variáveis/string
 	
 	printf("Digite o CPF do usuário a ser cadastrado: \n\n\t"); //Coletando informações do usuário
 	scanf("%s",cpf); //Salva a string cpf. "%s" refere-se à string
@@ -57,15 +60,28 @@ int registrar() //Função responsável pelo cadastro de usuários no sistema
 	fclose(file); //Fecha o arquivo
 	
 	printf("\n"); //Espaço-linha
-	
 	system("pause"); //Pausa a tela
+	
+	system("cls"); //Limpa a tela
+	printf("Deseja cadastrar outro usuário? S ou N? \n\n"); //Coletando informações do usuário
+	scanf(" %c", &opcao); //Salva a resposta do usuário na variável opção. "%c" refere-se à caracter
+	printf("\n"); //Espaço-linha
+	system("cls"); //Limpa a tela
+	
+	if (opcao == 'S' || opcao == 's') //Condicional que determina a ação a ser tomada com base na opção do usuário
+		return registrar(); //Retorna ao início da função
+	else //Condicional que determina a ação a ser tomada caso a opção do usuário seja diferente da determinada em IF
+		return 0; //Retorna à função principal
 	
 } //Fim da função registrar
 
 int consultar() //Função responsável pela consulta de usuários no sistema
 {
-	char cpf[40]; //Início da criação de variáveis/string
-	char conteudo[200]; //Final da criação de variáveis/string 
+	//Início da criação de variáveis/string
+	char cpf[40];
+	char conteudo[200]; 
+	char opcao;
+	//Final da criação de variáveis/string 
 	
 	printf("Digite o CPF do usuário a ser consultado: \n\n\t"); //Coletando informações do usuário
 	scanf("%s",cpf); //Salva a string cpf. "%s" refere-se à string
@@ -76,17 +92,28 @@ int consultar() //Função responsável pela consulta de usuários no sistema
 	if(file == NULL) //Determina ação condicional
 	{
 		printf("\nNão foi possível abrir o arquivo. Usuário não localizado.\n\n"); //Mensagem ao usuário
-	} //Fim do comando
+		
+	} //Final da ação condicional
 	
 	if(file != NULL) //Determina ação condicional
 	{
 		printf("\nEssas são as informações do usuário: \n\n"); //Mensagem ao usuário
-	} //Fim do comando
+	} //Final da ação condicional
 	
 	while(fgets(conteudo,200,file) != NULL) //Determina ação condicional
 	{
 		printf("%s",conteudo); //Exibe ao usuário as informações encontradas
-	} //Fim do comando
+	} //Final da ação condicional
+	
+	printf("Deseja consultar outro usuário? S ou N?\n\n"); //Coletando informações do usuário
+	scanf(" %c", &opcao); //Salva a resposta do usuário na variável opção
+	printf("\n"); //Espaço-linha
+	system("cls"); //Limpa a tela
+	
+	if (opcao=='S'||opcao=='s') //Determina ação condicional
+		return consultar(); //Retorna ao início da função
+	else 
+		return 0; //Final da ação condicional
 	
 	system("pause"); //Pausa a tela
 	fclose(file); //Fecha o arquivo
@@ -95,32 +122,73 @@ int consultar() //Função responsável pela consulta de usuários no sistema
 
 int deletar() //Função responsável pela exclusão de usuários no sistema
 {
-	char cpf[40]; //Criação da variável/string
+	//Início da criação de variáveis/string
+	char cpf[40];
+	char conteudo[200];
+	char opcao;
+	//Final da criação de variáveis/string 
 	
 	printf("Digite o CPF do usuário a ser deletado: \n\n\t"); //Coletando informações do usuário
 	scanf("%s",cpf); //Salva a string cpf. "%s" refere-se à string
 	
 	FILE *file; //Cria o arquivo
-	file = fopen(cpf,"r"); //Lê um arquivo existente.
+	file = fopen(cpf,"r"); //Lê um arquivo existente
 		
 	if(file == NULL) //Determina ação condicional
 	{
 		printf("\nO usuário não pôde ser localizado.\n\n"); //Mensagem ao usuário
 		system("pause"); //Pausa a tela
 		return 0;
-	} //Fim do comando
+	} //Final da ação condicional
 	
 	else //Determina ação condicional
-		fclose(file); //Fecha o arquivo
-		remove(cpf); //Deleta o arquivo
-		printf("\nO usuário foi deletado com sucesso.\n\n"); //Mensagem ao usuário
-		system("pause"); //Pausa a tela
+		while(fgets(conteudo,200,file) != NULL) //Determina ação condicional
+		{
+			printf("%s",conteudo); //Exibe ao usuário as informações encontradas
+		} //Final da ação condicional
+		printf("Tem certeza que deseja deletar este usuário? S ou N? \n\n"); //Coletando informações do usuário
+		scanf(" %c",&opcao); //Salva a resposta do usuário na variável opção
+		
+		switch (opcao) //Determina ação condicional
+		{
+			//Início do case
+			case 'S': //Compara resposta do usuário
+			case 's': //Compara resposta do usuário
+			fclose(file); //Fecha o arquivo
+			remove(cpf); //Deleta o arquivo
+			printf("\nO usuário foi deletado com sucesso.\n\n\n"); //Mensagem ao usuário
+			printf("Deseja deletar outro usuário? S ou N?\n\n"); //Coletando informações do usuário
+			scanf(" %c", &opcao); //Salva a resposta do usuário na variável opção
+			printf("\n"); //Espaço-linha
+			system("cls"); //Limpa a tela
+			
+			if (opcao=='S'||opcao=='s') //Determina ação condicional
+				return deletar(); //Retorna ao início da função
+			else //Determina ação condicional
+				return 0; //Retorna à função principal
+			system("pause"); //Pausa a tela
+			break; 
+			//Final do case
+			
+			//Início do case
+			case 'N': //Compara resposta do usuário
+			case 'n': //Compara resposta do usuário
+			return deletar(); //Retorna ao início da função
+			break;
+			//Final do case
+			
+			//Início do case
+			default: //Função padrão quando o comando do usuário não se encontra nas opções
+			printf("Esta opção não está disponível \n"); //Mensagem ao usuário
+			system("pause"); //Pausa a tela
+			break;
+			//Final do case
+		}//Final da ação condicional
 		
 } //Fim da função deletar
 
 int main() //Função principal do software
 {
-	
 	int opcao=0; //Definindo variáveis
 	int laco=1; //Definindo variáveis em loop
 	
@@ -131,18 +199,21 @@ int main() //Função principal do software
 		
 		setlocale(LC_ALL, "Portuguese"); //Definindo a linguagem
 	
-		printf("### Cartório da EBAC ###\n\n"); //Início do menu
+		 //Início do menu
+		printf("### Cartório da EBAC ###\n\n");
 		printf("Escolha a opção desejada do menu:\n\n");
 		printf("\t1 - Registrar nomes\n");
 		printf("\t2 - Consultar nomes\n");
 		printf("\t3 - Deletar nomes\n\n"); 
-		printf("Opção: "); //Fim do menu
+		printf("Opção: "); 
+		//Fim do menu
 	
 		scanf("%d", &opcao); //Armazenando a escolha do usuário na string
 	
 		system("cls"); //Limpa a tela
 		
-		switch(opcao) //Início da seleção do menu
+		//Início da seleção do menu
+		switch(opcao)
 		{
 			case 1: //Início da opção
 			registrar(); //Chamada de funções
@@ -161,9 +232,6 @@ int main() //Função principal do software
 			system("pause"); //Pausa a tela
 			break; //Fim do comando
 		} //Fim da seleção do menu
-	}
-	
-	
-	
+	}//Fim do laço
 	
 } //Fim da função principal
